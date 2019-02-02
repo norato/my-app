d9b497a30ae283f5a211abc8a248bc1553314f91
Felipe Norato Lacerda <fnoratol@gmail.com>
2019-02-02 13:04:27 -0200

    Define authenticated endpoints

--- lib/my_app_web/router.ex
+++ lib/my_app_web/router.ex
@@ -3,11 +3,32 @@ defmodule MyAppWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :fetch_session
+  end
+
+  pipeline :api_auth do
+    plug :ensure_authentication
   end
 
   scope "/api", MyAppWeb do
     pipe_through :api
-    resources "/users", UserController, except: [:new, :edit]
     post "/users/sign_in", UserController, :sign_in
+  end
+
+  scope "/api", MyAppWeb do
+    pipe_through [:api, :api_auth]
+    resources "/users", UserController, except: [:new, :edit]
+  end
+
+  def ensure_authentication(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> render(MyAppWeb.ErrorView, "401.json", message: "Unauthozired user")
+    end
   end
 end
